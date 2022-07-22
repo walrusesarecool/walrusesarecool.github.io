6b831b00581ecc7899ceaf1becc4fecfb9b73cba
walrusesarecool <rubyterren@gmail.com>
2022-07-22 00:07:50 -0700

    Bug fix

--- transition.js
+++ transition.js
@@ -4,14 +4,14 @@ let transitioning = false
 let firstLoad = false
 
 // If we're reloading the page, go back to the last page we were on
-const transitionList = {
+const routerPathes = {
   "" : "index.html",
   "index" : "index.html",
   "projects" : "projects.html"
 }
 
-if (currentPage in Object.keys(transitionList)) {initiateTransition(`${currentPage}.html`)}
-else if (currentPage in Object.values(transitionList)) {initiateTransition(currentPage)}
+if (currentPage in Object.keys(routerPathes)) {initiateTransition(routerPathes[currentPage])}
+else if (currentPage in Object.values(routerPathes)) {initiateTransition(currentPage)}
 
 document.addEventListener("click", clickHandler)
 window.addEventListener("popstate", popstateHandler)
